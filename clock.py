9375d8590a52ae6a27d27d0f2e4bfbd7a5660040
Mai-Hsuan Chia <j84255801912@gmail.com>
2017-05-12 08:51:46 +0000

    Test Scheduler

--- clock.py
+++ clock.py
@@ -1,9 +1,19 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
 
+from nearest_ubike_station.models import Station
+from .utils import get_ubike_stations_data, update_data
+
+
 sched = BlockingScheduler()
+
+def update_ubike_stations_data():
+
+    data = get_ubike_stations_data()
+    update_data(data)
 
 @sched.scheduled_job('interval', minutes=1)
 def timed_job():
-    print('This job is run every three minutes.')
+    update_ubike_stations_data()
+    print("[UPDATED] updated ubike stations data.")
 
 sched.start()
