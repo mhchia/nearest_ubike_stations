10684a0c613f813ba9ae329935b2a007527874ea
Mai-Hsuan Chia <j84255801912@gmail.com>
2017-05-16 03:33:14 +0000

    Remove uesless files

--- clean_table.py
+++
@@ -1,3 +0,0 @@
-from nearest_ubike_station.models import Station
-
-Station.objects().all().delete()
--- cron.py
+++
@@ -1,8 +0,0 @@
-from nearest_ubike_station.models import Station
-from utils import get_ubike_stations_data, update_data
-
-
-def update_ubike_stations_data():
-
-    data = get_ubike_stations_data()
-    update_data(data)
